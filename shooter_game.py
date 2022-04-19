db66a49ea0f5965dbb638dd0bf18b97e1dae42aa
scarlette0305 <103047527+scarlette0305@users.noreply.github.com>
2022-04-19 19:41:35 +0700

    Add files via upload

--- shooter_game.py
+++ shooter_game.py
@@ -1,16 +1,14 @@
 from pygame import *
 from random import randint
 
-#region variables
+#region creating variables
 _WIDTH = 800
 _HEIGHT = 640
-live = 20
-score = 0
 window = display.set_mode((_WIDTH,_HEIGHT))
 clock = time.Clock()
 #endregion
 
-
+#region creating classes
 class GameSprite(sprite.Sprite):
     def __init__(self, filename, x,y, width, height,speed=0):
         super().__init__()
@@ -23,11 +21,13 @@ class GameSprite(sprite.Sprite):
 
 class Player(GameSprite):
     def update(self):
+        # move the player to the left or right
         keys = key.get_pressed()
         if keys[K_LEFT]:
             self.rect.x -= self.speed
         if keys[K_RIGHT]:
             self.rect.x += self.speed
+        # control the player inside the window (not letting it goes out)
         if self.rect.x < 0 :
             self.rect.x = 0
         if self.rect.x > _WIDTH - self.rect.width:
@@ -40,7 +40,9 @@ class Player(GameSprite):
 class Enemy(GameSprite):
     def update(self):
         global live
+        # make the enemies move downwards
         self.rect.y += self.speed
+        # if the enemies get out of the window, minus one point
         if self.rect.top > _HEIGHT:
             self.rect.x = randint(0, _WIDTH- self.rect.width)
             self.rect.y = 0
@@ -58,39 +60,43 @@ class Asteroid(GameSprite):
         if self.rect.top > _HEIGHT:
             self.rect.x = randint(0, _WIDTH- self.rect.width)
             self.rect.y = 0
+
+#endregion
         
 #region trivials      
 
 live = 20
 score = 0
+# creating groups for each character
+antas = sprite.Group()
+fighter = sprite.Group() 
+asteroids = sprite.Group()
+# font and words
 font.init()
 f = font.SysFont(None, 70)
 live_remain = f.render("Live remaining:" + str(live), True, (0,0,0))
 score_having = f.render("Score: " + str(score), True, (0,255,255))
-introductory_sentence = f.render("You are evils and you're taking over the heaven", True, (255,255,255))
-introductory_sentence2 = f.render("Press the space bar to shoot the angels", True, (255,255,255))
-introductory_sentence0 = f.render("Press 'ENTER' to start playing", True, (255,255,255))
-antas = sprite.Group()
-fighter = sprite.Group() 
-asteroids = sprite.Group()
-introduction = GameSprite("welcome.png",0,0,_WIDTH,_HEIGHT)   
+# applying the pictures into the window
 game_over = GameSprite("lose_background.jpg",0,0,_WIDTH,_HEIGHT)
 game_win = GameSprite('game_win.png',0,0,_WIDTH,_HEIGHT)
 background = GameSprite("heaven.jpg", 0,0,_WIDTH, _HEIGHT)
-prota = Player("evil.png", _WIDTH/2, _HEIGHT -95, 60,80, 10)      
+prota = Player("evil.png", _WIDTH/2, _HEIGHT -95, 60,80, 10)    
+# create the function for creating the enemies  
 def create_anta():
     rand_speed = randint(1,3)
     anta = Enemy("angel.png", x=0, y=0, width=80,height=80,speed=rand_speed)
     anta.rect.x = randint(0, _WIDTH - anta.rect.width)
     anta.rect.bottom = 0
     antas.add(anta)
+for i in range(8):
+    create_anta()
+# creating the function for creating the aliens
 def create_asteroid():
-    asteroid = Asteroid("alien.png", x=0, y=0, width=80,height=80,speed=1)
+    rand_speed = randint(1,2)
+    asteroid = Asteroid("alien.png", x=0, y=0, width=80,height=80,speed=rand_speed)
     asteroid.rect.x = randint(0, _WIDTH - asteroid.rect.width)
     asteroid.rect.bottom = 0
     asteroids.add(asteroid)
-for i in range(8):
-    create_anta()
 for i in range(5):
     create_asteroid()
 #endregion
@@ -100,24 +106,12 @@ score = 0
 death = False
 game_is_running = True
 
+#running the game
 while game_is_running:
+    if event.peek(QUIT):
+        game_is_running = False
+  
     
-    for e in event.get():
-        if e.type == QUIT:
-            game_is_running = False
-        if e.type == KEYDOWN and e.key == K_SPACE:
-            f1  = Bullet("broken heart.png", prota.rect.centerx , prota.rect.top, 40,35,speed=6)
-            fighter.add(f1)
-    
-    #introduction.draw(window)
-    #introductory_sentence = f.render("You are evils and you're taking over the heaven", True, (255,255,255))
-    #introductory_sentence2 = f.render("Press the space bar to shoot the angels", True, (255,255,255))
-    #introductory_sentence3 = f.render("Press 'ENTER' to start playing", True, (255,255,255))
-    #window.blit(introductory_sentence, (100,100))
-    #window.blit(introductory_sentence2, (150,150))
-    #window.blit(introductory_sentence3, (200,200))
-    #keys = key.get_pressed()
-    #if keys[K_e]:
     background.draw(window)
     if not death:            
         prota.draw(window)
@@ -129,35 +123,50 @@ while game_is_running:
         live_remain = f.render("Live remaining:" + str(live), True, (0,0,0))
         score_having = f.render("Score: " + str(score), True, (0,255,255))
         window.blit(live_remain, (30,30))            
-        window.blit(score_having,(30,60))
+        window.blit(score_having,(30,80))
         asteroids.draw(window)
         asteroids.update()
         hits = sprite.groupcollide(fighter, antas, True,True)
         sprite.groupcollide(fighter, asteroids, True, False)
+        for hit in hits:
+            create_anta()
+            score += 1
     if sprite.spritecollide(prota, asteroids, False):
-        game_over.draw(window)
+        death = True
+        live = 0
     if death:
         game_over.draw(window)
-    for hit in hits:
-        create_anta()
-        score += 1
+    
     if live <= 0:
         game_over.draw(window) 
         keys = key.get_pressed()  
         if keys[K_r]:
             death = False
+            live = 20
+            score = 0
             antas.empty()
             for i in range(6):
                 create_anta()
-    if score == 20:
+            asteroids.empty()
+            for i in range(5):
+                create_asteroid()
+    if score == 30:
         game_win.draw(window)
         keys = key.get_pressed()  
-        if keys[K_d]:
+        if keys[K_r]:
             death = False
+            live = 20
+            score = 0
             antas.empty()
             for i in range(6):
                 create_anta()
-                
+            asteroids.empty()
+            for i in range(5):
+                create_asteroid()
+    for e in event.get():
+        if e.type == KEYDOWN and e.key == K_SPACE:
+            f1  = Bullet("broken heart.png", prota.rect.centerx , prota.rect.top, 40,35,speed=6)
+            fighter.add(f1)            
     display.update()
     clock.tick(60)
 
